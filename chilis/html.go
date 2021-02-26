93992d87d2f8c9731c90223363d427c8ccfde8de
cnnrmnn <connor.mann@outlook.com>
2021-02-26 15:12:24 +0000

    Fix syntax and style issues in html.go

--- chilis/html.go
+++ chilis/html.go
@@ -10,10 +10,10 @@ import (
 
 // findOne finds and returns the first HTML element with the given name and
 // class attribute
-func findOne(node *html.Node, name, class string) (node *html.Node, error) {
+func findOne(node *html.Node, name, class string) (*html.Node, error) {
 	query := fmt.Sprintf("//%s[@class='%s']", name, class)
 	element := htmlquery.FindOne(node, query)
-	if (element == nil) {
+	if element == nil {
 		return nil, errors.New("no matching elements found")
 	}
 	return element, nil
@@ -23,7 +23,7 @@ func findOne(node *html.Node, name, class string) (node *html.Node, error) {
 // attribute and returns its inner text.
 func innerText(node *html.Node, name, class string) (string, error) {
 	element, err := findOne(node, name, class)
-	if (err != nil) {
+	if err != nil {
 		return "", fmt.Errorf("failed to get inner text: %v", err)
 	}
 	innerText := htmlquery.InnerText(element)
