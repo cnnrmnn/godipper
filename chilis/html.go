e22ad022ceca3c625c516d664d769f2b92b64f92
cnnrmnn <connor.mann@outlook.com>
2021-02-26 15:02:09 +0000

    Consolidate HTML parsing utilities

--- chilis/html.go
+++ chilis/html.go
@@ -8,20 +8,24 @@ import (
 	"golang.org/x/net/html"
 )
 
-// classQuery returns an XPath query for an HTML element with the given name and
-// class attribute.
-func classQuery(name string, class string) string {
-	return fmt.Sprintf("//%s[@class='%s']", name, class)
+// findOne finds and returns the first HTML element with the given name and
+// class attribute
+func findOne(node *html.Node, name, class string) (node *html.Node, error) {
+	query := fmt.Sprintf("//%s[@class='%s']", name, class)
+	element := htmlquery.FindOne(node, query)
+	if (element == nil) {
+		return nil, errors.New("no matching elements found")
+	}
+	return element, nil
 }
 
 // innerText finds the first HTML element with the given name and class
 // attribute and returns its inner text.
-func innerText(node *html.Node, name string, class string) (string, error) {
-	query := classQuery(name, class)
-	tag := htmlquery.FindOne(node, query)
-	if tag == nil {
-		return "", errors.New("no matching elements found")
+func innerText(node *html.Node, name, class string) (string, error) {
+	element, err := findOne(node, name, class)
+	if (err != nil) {
+		return "", fmt.Errorf("failed to get inner text: %v", err)
 	}
-	innerText := htmlquery.InnerText(tag)
+	innerText := htmlquery.InnerText(element)
 	return innerText, nil
 }
