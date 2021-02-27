81901f8b11891c6d39ce36f57574366aa429006d
cnnrmnn <connor.mann@outlook.com>
2021-02-27 21:57:56 +0000

    Add JSON field tags to address struct

--- chilis/address.go
+++ chilis/address.go
@@ -4,11 +4,11 @@ import "fmt"
 
 // An Address is a United States address.
 type Address struct {
-	Street string
-	Unit   string
-	City   string
-	State  string
-	Zip    string
+	Street string `json:"street"`
+	Unit   string `json:"unit"`
+	City   string `json:"city"`
+	State  string `json:"state"`
+	Zip    string `json:"zip"`
 }
 
 // String returns a string representation of an Address (doesn't include Unit).
