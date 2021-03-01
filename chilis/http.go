10f9f91d15be2bf329e8a16ce022cf7e5eb62603
cnnrmnn <connor.mann@outlook.com>
2021-03-01 02:48:47 +0000

    Add utility to create jar with secure options

--- chilis/http.go
+++ chilis/http.go
@@ -9,21 +9,33 @@ import (
 	"golang.org/x/net/publicsuffix"
 )
 
-// createJar creates and returns a Jar with the given session cookie in it.
-func createJar(session *http.Cookie) (*cookiejar.Jar, error) {
+// createJar creates and returns a cookie jar with secure options (public
+// suffix list set)
+func createJar() (*cookiejar.Jar, error) {
 	options := &cookiejar.Options{PublicSuffixList: publicsuffix.List}
 	jar, err := cookiejar.New(options)
 	if err != nil {
-		return nil, fmt.Errorf("adding TripleDipper to cart: %v")
+		return nil, fmt.Errorf("creating cookie jar: %v")
+	}
+	return jar, nil
+}
+
+// createJar creates and returns a Jar with the given session cookie in it.
+func createSessionJar(session *http.Cookie) (*cookiejar.Jar, error) {
+	jar, err := createJar()
+	if err != nil {
+		return nil, fmt.Errorf("creating session cookie jar: %v")
 	}
 	u, err := url.Parse("https://www.chilis.com")
 	if err != nil {
-		return nil, fmt.Errorf("creating CookieJar: %v", err)
+		return nil, fmt.Errorf("creating session cookie jar: %v", err)
 	}
 	jar.SetCookies(u, []*http.Cookie{session})
 	return jar, nil
 }
 
+// createClient creates and returns a client with a jar with the given session
+// cookie in it.
 func createClient(session *http.Cookie) (*http.Client, error) {
 	jar, err := createJar(session)
 	if err != nil {
