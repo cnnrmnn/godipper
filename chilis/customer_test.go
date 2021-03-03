7a6f1b50efa3b50670fe6f1d83a250385b3fee19
cnnrmnn <connor.mann@outlook.com>
2021-03-03 16:45:45 -0500

    Add estimate tests

--- chilis/customer_test.go
+++ chilis/customer_test.go
@@ -1,7 +1,9 @@
 package chilis
 
 import (
+	"errors"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/antchfx/htmlquery"
@@ -37,6 +39,15 @@ var tidTests = []string{
 	"fe1c8b07-d8f5-4bb6-91c0-dfe497fa0ecf",
 	"8d772a03-3015-477e-bdfc-c0f147ec393f",
 	"61a205ae-98f5-48af-98ba-7471e74907be",
+}
+
+var estimateTests = []struct {
+	path string
+	time string
+}{
+	{"testdata/estimate1.json", "2021-03-03T21:37:43.258000Z"},
+	{"testdata/estimate2.json", "2021-03-03T21:42:26.730000Z"},
+	{"testdata/estimate3.json", "2021-03-03T21:58:00.234000Z"},
 }
 
 func init() {
@@ -92,5 +103,43 @@ func TestParseTransactionID(t *testing.T) {
 		if test != tid {
 			t.Errorf("%s: tid = %s, want %s", path, tid, test)
 		}
+	}
+}
+
+func TestParseEstimate(t *testing.T) {
+	for _, test := range estimateTests {
+		path := test.path
+		body, err := os.ReadFile(path)
+		if err != nil {
+			t.Errorf("%s: %v", path, err)
+		}
+		time, err := parseEstimate(body)
+		if err != nil {
+			t.Errorf("%s: %v", path, err)
+		}
+		if time != test.time {
+			t.Errorf("%s: time = %s, want %s", path, time, test.time)
+		}
+	}
+}
+
+func TestParseEstimateRange(t *testing.T) {
+	// No need to do more than one
+	path := "testdata/estimater1.json"
+	reason := "address is out of range"
+	body, err := os.ReadFile(path)
+	if err != nil {
+		t.Errorf("%s: %v", path, err)
+	}
+	_, err = parseEstimate(body)
+	if err == nil {
+		t.Errorf("%s: err = nil, want %s", path, reason)
+	}
+	var e ForbiddenError
+	if !errors.As(err, &e) {
+		t.Errorf("%s: got err = %v, want (ForbiddenError) %s", path, err, reason)
+	}
+	if e.Reason != reason {
+		t.Errorf("%s: err = %v, want %s", path, err, reason)
 	}
 }
