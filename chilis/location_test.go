cb1fb17d643c3b72422e1439f21d97dbad1a9621
cnnrmnn <connor.mann@outlook.com>
2021-03-04 11:17:56 -0500

    Add more location tests

--- chilis/location_test.go
+++ chilis/location_test.go
@@ -1,6 +1,7 @@
 package chilis
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/antchfx/htmlquery"
@@ -13,6 +14,16 @@ var nearestTests = []struct {
 	{"testdata/location1.html", "001.005.0945"},
 	{"testdata/location2.html", "001.005.1094"},
 	{"testdata/location3.html", "001.005.1320"},
+}
+
+var noneTests = []string{
+	"testdata/location_none1.html",
+	"testdata/location_none2.html",
+}
+
+var noDeliveryTests = []string{
+	"testdata/location_no_delivery1.html",
+	"testdata/location_no_delivery2.html",
 }
 
 func TestParseNearestID(t *testing.T) {
@@ -28,6 +39,42 @@ func TestParseNearestID(t *testing.T) {
 		}
 		if id != test.id {
 			t.Errorf("%s: id = %s, want %s", path, id, test.id)
+		}
+	}
+}
+
+func TestParseNearestIDNone(t *testing.T) {
+	for _, test := range noneTests {
+		reason := "no locations in proximity"
+		doc, err := htmlquery.LoadDoc(test)
+		if err != nil {
+			t.Errorf("%s: %v", test, err)
+		}
+		_, err = parseNearestID(doc)
+		if err == nil {
+			t.Errorf("%s: err = nil, want (ForbiddenError) %s", test, reason)
+		}
+		var e ForbiddenError
+		if !errors.As(err, &e) || reason != e.Reason {
+			t.Errorf("%s: err = %v, want (ForbiddenError) %s", test, err, reason)
+		}
+	}
+}
+
+func TestParseNearestIDNoDelivery(t *testing.T) {
+	for _, test := range noDeliveryTests {
+		reason := "location doesn't deliver"
+		doc, err := htmlquery.LoadDoc(test)
+		if err != nil {
+			t.Errorf("%s, %v", test, err)
+		}
+		_, err = parseNearestID(doc)
+		if err == nil {
+			t.Errorf("%s: err = nil, want (ForbiddenError) %s", test, reason)
+		}
+		var e ForbiddenError
+		if !errors.As(err, &e) || reason != e.Reason {
+			t.Errorf("%s: err = %s, want (ForbiddenError) %s", test, err, reason)
 		}
 	}
 }
