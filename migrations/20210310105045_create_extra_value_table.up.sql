4212d8f333d5e0798a1cd24f12a8dda364790bd2
cnnrmnn <connor.mann@outlook.com>
2021-03-10 11:18:17 -0500

    Move extra table population into create migration

--- migrations/20210310105045_create_extra_value_table.up.sql
+++ migrations/20210310105045_create_extra_value_table.up.sql
@@ -3,3 +3,13 @@ CREATE TABLE extra_value (
     extra_value VARCHAR(50) UNIQUE NOT NULL,
     CONSTRAINT pk_extra_value PRIMARY KEY (extra_value_id)
 );
+
+INSERT INTO extra_value
+    (extra_value)
+VALUES
+    ('Ancho-Chile Ranch Dressing'),
+    ('Avocado-Ranch Dressing'),
+    ('Bleu Cheese Dressing'),
+    ('Honey-Mustard Dressing'),
+    ('Original BBQ Sauce'),
+    ('Ranch Dressing');
--- migrations/20210310105338_populate_extra_value_table.up.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO extra_value
-    (extra_value)
-VALUES
-    ('Ancho-Chile Ranch Dressing'),
-    ('Avocado-Ranch Dressing'),
-    ('Bleu Cheese Dressing'),
-    ('Honey-Mustard Dressing'),
-    ('Original BBQ Sauce'),
-    ('Ranch Dressing');
