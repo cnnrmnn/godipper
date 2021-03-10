8ad1263f9c43e1eadc5f487f094129eb517f2bfd
cnnrmnn <connor.mann@outlook.com>
2021-03-09 20:31:34 -0500

    Add triple dipper table

---
+++ migrations/20210309202822_create_triple_dipper_table.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE triple_dipper;
---
+++ migrations/20210309202822_create_triple_dipper_table.up.sql
@@ -0,0 +1,4 @@
+CREATE TABLE triple_dipper (
+    triple_dipper_id SMALLINT UNSIGNED AUTO_INCREMENT,
+    CONSTRAINT pk_triple_dipper PRIMARY KEY (triple_dipper_id)
+);
