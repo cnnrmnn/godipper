e95bc41e23c0a4d517d5ae1d4c34a11849c85f33
cnnrmnn <connor.mann@outlook.com>
2021-03-10 11:16:59 -0500

    Move item table population into create migration

--- migrations/20210310103606_create_item_value_table.up.sql
+++ migrations/20210310103606_create_item_value_table.up.sql
@@ -3,3 +3,24 @@ CREATE TABLE item_value (
     item_value VARCHAR(50) UNIQUE NOT NULL,
     CONSTRAINT pk_item_value PRIMARY KEY (item_value_id)
 );
+
+INSERT INTO item_value 
+    (item_value)
+VALUES
+    ('Awesome Blossom Petals'),
+    ('Big Mouth® Bites'),
+    ('Boneless Buffalo Wings'),
+    ('Boneless Honey-Chipotle Wings'),
+    ('Boneless House BBQ Wings'),
+    ('Boneless Mango-Habanero Wings'),
+    ('Buffalo Wings'),
+    ('Crispy Cheddar Bites'),
+    ('Crispy Chicken Crispers'),
+    ('Crispy Honey-Chipotle Chicken Crispers®'),
+    ('Crispy Mango-Habanero Crispers®'),
+    ('Fried Pickles'),
+    ('Honey-Chipotle Wings'),
+    ('House BBQ Wings'),
+    ('Mango-Habanero Wings'),
+    ('Original Chicken Crispers®'),
+    ('Southwestern Eggrolls');
